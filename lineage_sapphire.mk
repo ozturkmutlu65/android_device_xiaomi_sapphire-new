9cab73e03d786ebdee3cb6eea5658d9dec540df3
Mutlu Öztürk <155987147+ozturkmutlu65@users.noreply.github.com>
2025-12-13 10:56:48 +0300

    sapphire: Add lineage_sapphire.mk for device configuration

---
+++ lineage_sapphire.mk
@@ -0,0 +1,38 @@
+#
+# Copyright (C) 2024 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from sapphire device
+$(call inherit-product, device/xiaomi/sapphire/device.mk)
+
+# Inherit some common Lineage stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Include our private certificate
+-include vendor/lineage-priv/keys/keys.mk
+
+# Device configs
+TARGET_BOOT_ANIMATION_RES = 1080
+TARGET_HAS_UDFPS := true
+NO_APERTURE := true
+
+# EPPE
+TARGET_DISABLE_EPPE := true
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+PRODUCT_NAME := lineage_sapphire
+PRODUCT_DEVICE := sapphire
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi Note 13
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BUILD_FINGERPRINT := Redmi/sapphiren_global/sapphiren:13/TKQ1.221114.001/OS2.0.203.0.VNGMIXM:user/release-keys
